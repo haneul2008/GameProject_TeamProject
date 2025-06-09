a9fd88565cab470ad8ad977e74c02f8de09d59c7
machany <machanicaly@gmail.com>
2025-06-09 14:15:24 +0900

    .

--- GameProjectTeamProject/Object.h
+++ GameProjectTeamProject/Object.h
@@ -1,22 +1,27 @@
 #pragma once
 
+#include<Vector>
+#include<map>
+
 typedef struct _Position
 {
 public:
     int x;
     int y;
-} Position, * p_Position, Pos, * p_Pos;
+} Position, * pPosition, Pos, * pPos;
 
 // dead등 상태에 따른 이미지 추가 가능성 있음
-typedef struct _ObjectImage
+typedef struct _ObjectRenderInfo
 {
 public:
     char image;
-} Image, *p_Image;
+    std::map<char, std::vector<char>> animation;
+} RenderInfo, *pRenderInfo, RendI, *pRendI;
 
 typedef struct _Object
 {
 public:
-    _Position pos;
+    Pos pos;
+    RendI render;
+} Object, *pObject, Obj, *pObj;
 
-};
